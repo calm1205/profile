eb9a13d3a21588d06daa04658a66db0db831222f
calm1205 <authentic.nagi@gmail.com>
2024-11-23 10:19:41 +0000

    feat: set path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite"
+import { fileURLToPath } from "node:url"
 import react from "@vitejs/plugin-react"
+import path from "node:path"
 
-// https://vite.dev/config/
+const dirname = path.dirname(fileURLToPath(new URL(import.meta.url)))
 export default defineConfig({
   plugins: [react()],
   base: "./",
   build: {
     outDir: "publish",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(dirname, "src"),
+    },
   },
 })
