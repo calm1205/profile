e1540bf758804b68057bb580c984da5f15dd24b3
calm1205 <authentic.nagi@gmail.com>
2024-12-11 23:11:01 +0900

    fix: add sns link

--- src/components/materials/SnsIcons.tsx
+++ src/components/materials/SnsIcons.tsx
@@ -4,13 +4,16 @@ import { styled } from "styled-components"
 
 export const SnsIcons: React.FC = () => (
   <Container>
-    <Link href="#" target="_blank">
+    <Link href="https://x.com/nagi_calm1205" target="_blank">
       <IconX />
     </Link>
-    <Link href="#" target="_blank">
+    <Link
+      href="https://www.linkedin.com/in/%E5%87%AA-%E6%A3%AE%E5%B1%B1-45a77a288/"
+      target="_blank"
+    >
       <IconLinkedin />
     </Link>
-    <Link href="#" target="_blank">
+    <Link href="https://github.com/calm1205" target="_blank">
       <IconGithub />
     </Link>
   </Container>
